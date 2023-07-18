3ad32635d48a584927258bf7b594a2b8a1c1c662
Débora Almeida <debora.almeida.de.mello@gmail.com>
2023-07-18 20:40:22 -0300

    Adicionar projetos implementado

--- src/Interfaces/ITarefa.ts
+++ src/Interfaces/ITarefa.ts
@@ -1,5 +1,8 @@
+import IProjeto from "@/Interfaces/IProjeto";
+
 export default interface ITarefa {
     duracaoEmSegundos: number,
     descricao: string,
+    projeto: IProjeto
 }
 
--- src/components/Formulario.vue
+++ src/components/Formulario.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="box formulario">
     <div class="columns">
-      <div class="column is-8" role="form" aria-label="Formulário para criação de uma nova tag">
+      <div class="column is-5" role="form" aria-label="Formulário para criação de uma nova tag">
         <input
             type="text"
             class="input"
@@ -9,16 +9,32 @@
             v-model="descricao"
         />
       </div>
+      <div class="column is-3">
+        <div class="select">
+          <select v-model="idProjeto">
+            <option value="">Selecione o projeto</option>
+            <option
+                :value="projeto.id"
+                v-for="projeto in projetos"
+                :key="projeto.id"
+            >
+              {{ projeto.nome }}
+            </option>
+          </select>
+        </div>
+      </div>
       <div class="column">
-        <Temporizador @temporizadorFinalizado="finalizarTarefa"/>
+        <Temporizador @temporizadorFinalizado="salvarTarefa"/>
       </div>
     </div>
   </div>
 </template>
 
 <script lang="ts">
-  import { defineComponent } from "vue";
+import {computed, defineComponent} from "vue";
   import Temporizador from "@/components/Temporizador.vue";
+  import {useStore} from "vuex";
+  import {key} from "@/store";
 
   export default defineComponent({
     // eslint-disable-next-line vue/multi-word-component-names
@@ -27,16 +43,24 @@
     components: {Temporizador},
     data () {
       return {
-        descricao: ''
+        descricao: '',
+        idProjeto: ''
       }
     },
     methods: {
-      finalizarTarefa(tempoDecorrido: number): void {
+      salvarTarefa(tempoDecorrido: number): void {
         this.$emit('salvarTarefa', {
           duracaoEmSegundos: tempoDecorrido,
-          descricao: this.descricao
+          descricao: this.descricao,
+          projeto: this.projetos.find(proj => proj.id == this.idProjeto)
         })
         this.descricao = ''
+      }
+    },
+    setup () {
+      const store = useStore(key)
+      return {
+        projetos: computed(() => store.state.projetos)
       }
     }
   })
