57d81e1262768ee88e69fe8e15c0f040916d55a7
Débora Almeida <debora.almeida.de.mello@gmail.com>
2023-07-18 19:48:35 -0300

    Lista de projetos criada

--- src/views/Projetos.vue
+++ src/views/Projetos.vue
@@ -1,16 +1,68 @@
 <template>
-  <h1>Projetos</h1>
+  <section class="projetos">
+    <h1>Projetos</h1>
+    <form @submit.prevent="salvar">
+      <div class="field">
+        <label for="nomeDoProjeto" class="label">
+          Nome do Projeto
+        </label>
+        <input
+            type="text"
+            class="input"
+            v-model="nomeDoProjeto"
+            id="nomeDoProjeto"
+            required>
+      </div>
+      <div class="field">
+        <button class="button" type="submit">
+          Salvar
+        </button>
+      </div>
+    </form>
+    <table class="table is-fullwidth">
+      <thead>
+        <tr>
+          <th>ID</th>
+          <th>Nome</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="projeto in projetos" :key="projeto.id">
+          <td>{{ projeto.id }}</td>
+          <td>{{ projeto.nome }}</td>
+        </tr>
+      </tbody>
+    </table>
+  </section>
 </template>
 
 <script lang="ts">
 import {defineComponent} from "vue";
+import IProjeto from "@/Interfaces/IProjeto";
 export default defineComponent ({
   // eslint-disable-next-line vue/multi-word-component-names
   name: "Projetos",
-
+  data() {
+    return {
+      nomeDoProjeto: "",
+      projetos: [] as IProjeto[]
+    }
+  },
+  methods: {
+    salvar () {
+      const projeto: IProjeto = {
+        nome: this.nomeDoProjeto,
+        id: new Date().toISOString()
+      }
+      this.projetos.push(projeto)
+      this.nomeDoProjeto = ""
+    }
+  }
 })
 </script>
 
 <style scoped>
-
+  .projetos {
+    padding: 1.25rem;
+  }
 </style>
