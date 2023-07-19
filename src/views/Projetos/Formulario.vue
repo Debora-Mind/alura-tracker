c796c02945811ec8070ba4eba732f22cbca99c98
Débora Almeida <debora.almeida.de.mello@gmail.com>
2023-07-18 21:05:48 -0300

    Editar projeto implementado

--- src/views/Projetos/Formulario.vue
+++ src/views/Projetos/Formulario.vue
@@ -19,29 +19,26 @@
         </button>
       </div>
     </form>
-    <table class="table is-fullwidth">
-      <thead>
-        <tr>
-          <th>ID</th>
-          <th>Nome</th>
-        </tr>
-      </thead>
-      <tbody>
-        <tr v-for="projeto in projetos" :key="projeto.id">
-          <td>{{ projeto.id }}</td>
-          <td>{{ projeto.nome }}</td>
-        </tr>
-      </tbody>
-    </table>
   </section>
 </template>
 
 <script lang="ts">
-import {computed, defineComponent} from "vue";
+import {defineComponent} from "vue";
 import {useStore} from "@/store";
 export default defineComponent ({
   // eslint-disable-next-line vue/multi-word-component-names
-  name: "Projetos",
+  name: "Formulario",
+  props: {
+    id: {
+      type: String
+    }
+  },
+  mounted() {
+    if(this.id) {
+      const projeto = this.store.state.projetos.find(proj => proj.id == this.id)
+      this.nomeDoProjeto = projeto?.nome || ''
+    }
+  },
   data() {
     return {
       nomeDoProjeto: ""
@@ -49,15 +46,22 @@ export default defineComponent ({
   },
   methods: {
     salvar () {
-      this.store.commit('ADICIONA_PROJETO', this.nomeDoProjeto)
+      if (this.id) {
+        this.store.commit('ALTERA_PROJETO', {
+          id: this.id,
+          nome: this.nomeDoProjeto
+        })
+      } else {
+        this.store.commit('ADICIONA_PROJETO', this.nomeDoProjeto)
+      }
       this.nomeDoProjeto = ""
+      this.$router.push('/projetos')
     }
   },
   setup () {
     const store = useStore()
     return {
-      store,
-      projetos: computed(() => store.state.projetos)
+      store
     }
   }
 })
