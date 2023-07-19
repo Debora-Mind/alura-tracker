110dc948d1fd5137f27a09a43f837e659f6f6748
Débora Almeida <debora.almeida.de.mello@gmail.com>
2023-07-18 21:57:52 -0300

    Exclusão de Projetos implementado

--- src/views/Projetos.vue
+++ src/views/Projetos.vue
@@ -1,54 +1,21 @@
 <template>
-  <section class="projetos">
-    <h1>Projetos</h1>
-    <router-link to="projetos/novo" class="button">
-      <span class="icon is-small">
-        <i class="fas fa-plus"></i>
-      </span>
-      <span>Novo projeto</span>
-    </router-link>
-    <table class="table is-fullwidth">
-      <thead>
-        <tr>
-          <th>ID</th>
-          <th>Nome</th>
-          <th>Ações</th>
-        </tr>
-      </thead>
-      <tbody>
-        <tr v-for="projeto in projetos" :key="projeto.id">
-          <td>{{ projeto.id }}</td>
-          <td>{{ projeto.nome }}</td>
-          <td>
-            <router-link :to="`/projetos/${projeto.id}`" class="button">
-              <span class="icon is-small">
-                <i class="fas fa-pencil-alt"></i>
-              </span>
-            </router-link>
-          </td>
-        </tr>
-      </tbody>
-    </table>
-  </section>
+  <div class="projetos">
+    <h1 class="title">Projetos</h1>
+    <router-view></router-view>
+  </div>
 </template>
 
 <script lang="ts">
-import {computed, defineComponent} from "vue";
-import {useStore} from "@/store";
-export default defineComponent ({
+import {defineComponent} from "vue";
+
+export default defineComponent({
   // eslint-disable-next-line vue/multi-word-component-names
-  name: "Projetos",
-  setup () {
-    const store = useStore()
-    return {
-      projetos: computed(() => store.state.projetos)
-    }
-  }
+  name: 'Projetos',
 })
 </script>
 
 <style scoped>
-  .projetos {
-    padding: 1.25rem;
-  }
+.projetos {
+  padding: 1.25rem;
+}
 </style>
